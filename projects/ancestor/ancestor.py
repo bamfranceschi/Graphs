142b4358048ca7a45d6cfb75d5e1bf5e2d26a8db
bamfranceschi <anna.franceschi@gmail.com>
2020-07-14 16:02:54 -0500

    finished mvp for monday

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,23 @@
 
+def find_parents(child):
+    pass
+
+
 def earliest_ancestor(ancestors, starting_node):
     pass
+
+
+'''
+Thoughts:
+
+I think we want depth first.
+We start with an input child, and must trace back via parents to find the farthest ancestor
+Is this reversed?
+It's recursive!! takes in a dataset and an ID
+Need to know when a child doesn't have a parent- that's our base case. That's the root ancestor
+
+Let's think through how to make a helper function that finds our parents.
+
+Use a stack if I don't do recursion
+
+'''
